33f3a0f3474d88a4d5442842426847aafa8cee21
Luis Castro <luiscastro1991@gmail.com>
2012-11-06 15:19:01 +0000

    Project, app and Models

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from django.contrib.admin.models import User
+
+class Strategy(models.Model):
+  title = models.CharField(max_length=250)
+  vision = models.CharField(max_length=250)
+  user = models.ForeignKey('User')
+
+class Goal(models.Model):
+  title = models.CharField(max_length=250)
+  strategy = models.ForeignKey('Strategy')
+
+class Action(models.Model):
+  challenge = models.CharField(max_length=250)
+  start_date = models.DateTimeField()
+  goal = models.ForeignKey('Goal')
